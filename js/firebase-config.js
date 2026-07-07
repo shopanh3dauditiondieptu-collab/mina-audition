db4fe2a201a9cb1f5c1b37a922c23fe248c93488
shopanh3dauditiondieptu-collab <shopanh3daudition.dieptu@gmail.com>
2026-07-07 22:53:42 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD0bWkR6Dhdrg94JkTw5nWjH6LoydjR080",
-  authDomain: "minaaudition-13650.firebaseapp.com",
-  projectId: "minaaudition-13650",
-  storageBucket: "minaaudition-13650.firebasestorage.app",
-  messagingSenderId: "411681077748",
-  appId: "1:411681077748:web:ba18c9ee93527f5b7321a1",
-  measurementId: "G-3NY5WVRYHN"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { app, auth, db, storage };
+// Dán cấu hình Firebase của bạn vào đây khi cần.
+// Ví dụ:
+// const firebaseConfig = { apiKey:"...", authDomain:"...", projectId:"..." };
