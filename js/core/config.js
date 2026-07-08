54aa58b79bf573e736657d5d82d876b8134405eb
shopanh3dauditiondieptu-collab <shopanh3daudition.dieptu@gmail.com>
2026-07-09 06:24:51 +0700

    Create config.js

---
+++ js/core/config.js
@@ -0,0 +1,34 @@
+/* =====================================================
+   MINA CMS V7 - CORE CONFIG
+===================================================== */
+
+window.MINA_CONFIG = {
+  siteName: "Mina Audition",
+
+  hero: {
+    slogan: "Kênh này chỉ có NHẠC chill ♡",
+    title: "MINA AUDITION",
+    meta: "♫ DANCE • D8 TEAM • AUDITION VTC",
+    description:
+      "Chào mừng bạn đến với thế giới Audition cùng Mina! Nơi chia sẻ đam mê, kỹ năng và những khoảnh khắc cháy hết mình trên sàn nhảy ảo.",
+    buttonText: "▶ XEM YOUTUBE NGAY",
+    buttonLink: "https://www.youtube.com/@mina.audition"
+  },
+
+  social: {
+    youtube: "https://www.youtube.com/@mina.audition",
+    facebook: "https://www.facebook.com/mina.audition/",
+    tiktok: "https://www.tiktok.com/",
+    instagram: "https://www.instagram.com/"
+  },
+
+  footer: {
+    text: "© 2026 Mina Audition. Dance • Poppin • D8 Team."
+  },
+
+  seo: {
+    title: "Mina Audition | Dance • Poppin • D8 Team",
+    description:
+      "Website Mina Audition chia sẻ video, blog, wiki skill và nội dung cộng đồng Audition VTC."
+  }
+};
