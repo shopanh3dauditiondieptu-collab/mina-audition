581984753141791468948613ebd123b6cb6befdd
shopanh3dauditiondieptu-collab <shopanh3daudition.dieptu@gmail.com>
2026-07-11 07:14:40 +0700

    Create config.js

---
+++ js/admin/config.js
@@ -0,0 +1,11 @@
+export const ADMIN_EMAIL = "mina.auditionvtc@gmail.com";
+
+export const CLOUDINARY = {
+  cloudName: "rpwcnrfg",
+  uploadPreset: "mina-upload",
+  folder: "mina-review"
+};
+
+export const CATEGORIES_API = "/api/categories";
+export const ADMIN_KEY_SESSION = "mina_admin_api_key";
+export const AUTOSAVE_KEY = "minaCmsV3Draft";
